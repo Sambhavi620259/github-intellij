6aba2af083391b96316232d775e832bd159112fd
Satyamdev028 <sambhavishubhi580@gmail.com>
2025-12-19 01:57:47 +0530

    MailConfig Added

---
+++ src/main/java/in/bawvpl/Authify/config/MailConfig.java
@@ -0,0 +1,26 @@
+package in.bawvpl.Authify.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp-relay.brevo.com");
+        sender.setPort(587);
+        sender.setUsername("9bc3c2001@smtp-brevo.com");
+        sender.setPassword("YOUR_BREVO_SMTP_PASSWORD");
+
+        Properties props = sender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return sender;
+    }
+}
