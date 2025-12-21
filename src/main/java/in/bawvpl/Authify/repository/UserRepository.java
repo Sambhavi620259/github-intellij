f403f15be82112b3c586c8571f98c7a36bf6ef1a
Satyamdev028 <sambhavishubhi580@gmail.com>
2025-12-21 10:04:42 +0530

    Authify-login-logout-register-profile

--- src/main/java/in/bawvpl/Authify/repository/UserRepository.java
+++ src/main/java/in/bawvpl/Authify/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package in.bawvpl.Authify.repository;
 
-import java.util.Optional;
-
+import in.bawvpl.Authify.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import in.bawvpl.Authify.entity.UserEntity;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
     Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByPhoneNumber(String phoneNumber);
+
     boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
